9629107aeba4463eb160400d93fffeb071fbebb4
zertransport-dev <zertransport@gmail.com>
2026-09-15 02:34:02 +0200

    Refactor app.py to remove redundant imports

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-import subprocess
-import sys
 
-try:
-    import reportlab
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "reportlab"])
-    import reportlab
     import streamlit as st
 import pandas as pd
 import os
